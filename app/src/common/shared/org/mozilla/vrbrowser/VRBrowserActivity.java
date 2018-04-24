dde4ff2bb1d18b85bdebc4514e664532e86f5e37
Imanol Fernandez <mortimergoro@gmail.com>
2018-04-24 09:38:09 +0200

    Tray implementation

--- app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
+++ app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
@@ -51,6 +51,14 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
     static final int GestureSwipeRight = 1;
     static final int SwipeDelay = 1000; // milliseconds
 
+    static final int TrayEventHelp = 0;
+    static final int TrayEventSettings = 1;
+    static final int TrayEventPrivate = 2;
+    static final int TrayEventNew = 3;
+    static final int TrayEventNotification = 4;
+    static final int TrayEventHide = 5;
+    static final int TrayEventExit = 6;
+
     static final String LOGTAG = "VRB";
     HashMap<Integer, Widget> mWidgets;
     private int mWidgetHandleIndex = 1;
@@ -274,6 +282,17 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
                     mLastRunnable = new SwipeRunnable();
                     mHandler.postDelayed(mLastRunnable, SwipeDelay);
                 }
+            }
+        });
+    }
+
+    @Keep
+    void handleTrayEvent(final int aType) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(LOGTAG, "Tray event not implemented: " + aType);
+                mAudioEngine.playSound(AudioEngine.Sound.CLICK);
             }
         });
     }
